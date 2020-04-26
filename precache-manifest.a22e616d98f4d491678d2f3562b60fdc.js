ee16981b5239642c92e1b432b9472077ea2add3d
dmitrii <ffklffkl@gmail.com>
2020-04-26 19:38:57 +0300

    Updates

---
+++ precache-manifest.a22e616d98f4d491678d2f3562b60fdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76d90847d457ae5d549abda58bf63e3",
+    "url": "/movies-catalog/index.html"
+  },
+  {
+    "revision": "249e3e04c97d41813009",
+    "url": "/movies-catalog/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "249e3e04c97d41813009",
+    "url": "/movies-catalog/static/js/2.c4c81abb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/movies-catalog/static/js/2.c4c81abb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8532a6c36e50a982bcf",
+    "url": "/movies-catalog/static/js/main.8c758165.chunk.js"
+  },
+  {
+    "revision": "b574c857e7e0b7074507",
+    "url": "/movies-catalog/static/js/runtime-main.8e5bf343.js"
+  }
+]);
